71af3bf745302e82fb44d9247b8f3fe05f5da478
Daniel Flores <daniel.flores4930@gmail.com>
2025-05-23 13:03:25 -0400

    Initial commit

---
+++ JulesPoke/JulesPokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JulesPokeApp.swift
+//  JulesPoke
+//
+//  Created by Daniel Flores on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct JulesPokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JulesPokeTests/JulesPokeTests.swift
@@ -0,0 +1,17 @@
+//
+//  JulesPokeTests.swift
+//  JulesPokeTests
+//
+//  Created by Daniel Flores on 5/23/25.
+//
+
+import Testing
+@testable import JulesPoke
+
+struct JulesPokeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
